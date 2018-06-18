e4929a520c043edc3093b8c465b1275cd851242a
Goos van den Bekerom <goos.bekerom@gmail.com>
2018-06-18 19:06:04 +0200

    Added feed forward functionality for the neural network

--- nn.js
+++ nn.js
@@ -1,8 +1,44 @@
+var Matrix = require('./matrix')
+
+const sigmoid = x => 1 / (1 + Math.exp(-x))
+
 module.exports = class NeuralNetwork {
   constructor(input, hidden, output) {
     this.amountIn = input
     this.amountHidden = hidden
     this.amountOut = output
+
+    this.weightsIH = new Matrix(this.amountHidden, this.amountIn)
+    this.weightsHO = new Matrix(this.amountOut, this.amountHidden)
+    this.weightsIH.randomize()
+    this.weightsHO.randomize()
+
+    this.biasHidden = new Matrix(this.amountHidden, 1)
+    this.biasOut = new Matrix(this.amountOut, 1)
+    this.biasHidden.randomize()
+    this.biasOut.randomize()
+  }
+
+  feedforward(inputs) {
+    if (!(inputs instanceof Matrix)) {
+      if (inputs instanceof Array) inputs = Matrix.fromArray(inputs)
+      else {
+        console.error('FATAL: feedforward input should be an Array or a Matrix.')
+        return undefined
+      }
+    } 
+
+    // Generate hidden outputs
+    let hidden = Matrix.product(this.weightsIH, inputs)
+    hidden.add(this.biasHidden)
+    hidden.map(sigmoid)
+
+    // Generate outputs
+    let output = Matrix.product(this.weightsHO, hidden)
+    output.add(this.biasOut)
+    output.map(sigmoid)
+
+    return output
   }
 
   print() {
